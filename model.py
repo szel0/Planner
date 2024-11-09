f1d6696503ad96d92ecad2e96b9859b7ff59454a
Mateusz <szeleq528@gmail.com>
2024-11-09 15:13:10 +0100

    Fix in Calendar.add_task (error in task assignment to dates): Fixed the issue where tasks were incorrectly assigned to dates in the calendar. Now, tasks are properly grouped by their respective dates in the self.tasks dictionary.
    Fixed indentation in __repr__ method of Task class and get_tasks_for_day method:
    Corrected the indentation of the __repr__ method in the Task class.

--- model.py
+++ model.py
@@ -7,22 +7,22 @@ class Priority:
     CRITICAL = 4
 
 class Task:
-    def __init__ (self, title, description, date, priority = Priority.MEDIUM):
+    def __init__(self, title, description, date, priority=Priority.MEDIUM):
         self.title = title
         self.description = description
         self.date = datetime.strptime(date, "%Y-%m-%d")
         self.priority = priority
 
-        def __repr__(self):
-            return f"Task(title='{self.title}', description='{self.description}', date='{self.date.strftime('%Y-%m-%d')}' )"
+    def __repr__(self):
+        return f"Task(title='{self.title}', description='{self.description}', date='{self.date.strftime('%Y-%m-%d')}')"
 
 class Calendar:
     def __init__(self):
-        self.tasks = []
+        self.tasks = {}
 
     def add_task(self, task):
         if task.date not in self.tasks:
-            self.task[task.date] = []
+            self.tasks[task.date] = []
         self.tasks[task.date].append(task)
 
     def get_tasks_for_day(self, date):
